46ba4a2eb6709c39dc5fca15d19e2ca65ff2292d
abdul-ashad15 <abdul.ashad1@gmail.com>
2020-06-26 16:48:33 +0530

    2 files modified

--- src/app/login/signup.components.ts
+++ src/app/login/signup.components.ts
@@ -2,21 +2,47 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from './login.service';
 import { User } from './user';
-import  
+import  {FormGroup, FormBuilder, Validators} from '@angular/forms';
+
 @Component({
     selector: 'app-signup',
     templateUrl: './signup.components.html'
   })
 
 
-export class SignupComponent
+export class SignupComponent implements OnInit
 {
     title : string = "Sign Up";
-    user : User;
+    user : User = new User();
+    registerForm: FormGroup;
+    hide = true;
 
-    constructor(private router: Router, public loginservice : LoginService)
+    constructor(private router: Router, public loginservice : LoginService, private formBuilder: FormBuilder)
     {
 
+    }
+
+    ngOnInit()
+    {
+      this.registerForm = this.formBuilder.group({
+      'username': [this.user.username, [
+        Validators.required
+      ]],
+      'email': [this.user.email, [
+        Validators.required,
+        Validators.email
+      ]],
+      'password': [this.user.password, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(30)
+      ]],
+      'phone': [this.user.phone, [
+        Validators.required,
+         Validators.minLength(10),
+        Validators.maxLength(12)
+      ]],
+    });
     }
 
     login() : void
@@ -29,4 +55,8 @@ export class SignupComponent
         debugger;
         this.loginservice.registerUsers(currentuser).subscribe();
     }
+
+    onRegisterSubmit() {
+    alert(this.user.username + ' ' + this.user.email + ' ' + this.user.password + ' ' + this.user.phone);
+  }
 }
